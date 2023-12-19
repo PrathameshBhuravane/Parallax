71623cc4c8a75368b1128aa977857356ad4931a3
PrathameshBhuravane <bhuravaneprathamesh@gmail.com>
2023-12-19 18:21:59 +0530

    Initial Commit

---
+++ Parallax/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parallax
+//
+//  Created by Prathamesh on 19/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parallax/ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxApp.swift
+//  Parallax
+//
+//  Created by Prathamesh on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
